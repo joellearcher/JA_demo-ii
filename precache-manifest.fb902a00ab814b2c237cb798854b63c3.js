814428d01c37b7ef73dcd974b79d48700626caa7
joellearcher <joelle.archerdmello@gmail.com>
2020-06-10 20:09:15 +0100

    Updates

---
+++ precache-manifest.fb902a00ab814b2c237cb798854b63c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fc8b87149138a55d40cc2004472b9b",
+    "url": "/demo-ii/index.html"
+  },
+  {
+    "revision": "b2609a7a9d7a24f80e4f",
+    "url": "/demo-ii/static/css/main.b8c2a955.chunk.css"
+  },
+  {
+    "revision": "48abf624a08c60a385db",
+    "url": "/demo-ii/static/js/2.647d4c0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-ii/static/js/2.647d4c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2609a7a9d7a24f80e4f",
+    "url": "/demo-ii/static/js/main.df8df2d8.chunk.js"
+  },
+  {
+    "revision": "de36381db095e046a40a",
+    "url": "/demo-ii/static/js/runtime-main.d3603012.js"
+  },
+  {
+    "revision": "9887cc7bf5c901b07270a9f76de0c983",
+    "url": "/demo-ii/static/media/webdev_hp.9887cc7b.jpg"
+  }
+]);
